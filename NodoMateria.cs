d26efc8389935dd23e30d52b80fe2f7c225144c4
sofilunasalga546 <sofilunasalga546@gmail.com>
2026-04-04 13:24:39 -0500

    Agregando clases de materias y nodos

---
+++ NodoMateria.cs
@@ -0,0 +1,14 @@
+class Nodomateria
+{
+    public string nombre;
+    public double nota;
+    public NodoMateria siguiente;
+    
+public Nodomateria(string nombre , double nota)
+    {
+        this.nombre = nombre ;
+        this.nota = nota;
+        this.siguiente = null;
+
+    }
+}
